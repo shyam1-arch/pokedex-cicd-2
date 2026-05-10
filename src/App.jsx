06e66e29a0b5f64939cd65fac42f09eece8ec908
shyam1-arche <shyamyadav0816.sy@gmail.com>
2026-05-10 17:19:52 +0300

    fix app eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,21 @@
-import React from 'react'
-import { Routes, Route, useMatch } from 'react-router-dom'
-import { useApi } from './useApi'
-import LoadingSpinner from './LoadingSpinner'
-import ErrorMessage from './ErrorMessage'
-import PokemonPage from './PokemonPage'
-import PokemonList from './PokemonList'
+const express = require('express')
+const app = express()
 
-const mapResults = ({ results }) => results.map(({ url, name }) => ({
-  url,
-  name,
-  id: parseInt(url.match(/\/(\d+)\//)[1])
-}))
+const PORT = process.env.PORT || 5001
 
-const App = () => {
-  const match = useMatch('/pokemon/:name')
-  const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
+app.use(express.static('dist'))
 
-  if (isLoading) {
-    return <LoadingSpinner />
-  }
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 
-  if (error) {
-    return <ErrorMessage error={error} />
-  }
+app.get('/version', (req, res) => {
+  res.send('1')
+})
 
-  let next = null
-  let previous = null
-
-  if (match && match.params) {
-    const pokemonId = pokemonList.find(({ name }) => name === match.params.name).id
-    previous = pokemonList.find(({ id }) => id === pokemonId - 1)
-    next = pokemonList.find(({ id }) => id === pokemonId + 1)
-  }
-
-  return (
-    <Routes>
-      <Route path="/" element={<PokemonList pokemonList={pokemonList} />} />
-      <Route
-        path="/pokemon/:name"
-        element={<PokemonPage pokemonList={pokemonList} previous={previous} next={next} />}
-      />
-    </Routes>
-  )
+const start = async () => {
+  await app.listen(PORT)
+  console.log(`server started on port ${PORT}`)
 }
 
-export default App
+start()
